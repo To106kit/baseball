cf765077352089972a55da19168869ea4958f4cf
To106kit <t.to106ki@gmail.com>
2022-12-12 20:32:51 +0900

    投手版mysqlデータベース取得スクリプト作成前コミット

--- src/getdata/main.py
+++ src/getdata/main.py
@@ -62,7 +62,7 @@ t_unit_flag = "sec"
 # データ取得対象球団のベースとなるページurlリスト
 ## 打者url
 t_url_batter_list = [
-        "https://baseballdata.jp/1/ctop.html",
+        # "https://baseballdata.jp/1/ctop.html",
         # "https://baseballdata.jp/2/ctop.html",
         # "https://baseballdata.jp/3/ctop.html",
         # "https://baseballdata.jp/4/ctop.html",
@@ -79,17 +79,17 @@ t_url_batter_list = [
 ## 投手url
 t_url_pitcher_list = [
         "https://baseballdata.jp/1/cptop.html",
-        "https://baseballdata.jp/2/cptop.html",
-        "https://baseballdata.jp/3/cptop.html",
-        "https://baseballdata.jp/4/cptop.html",
-        "https://baseballdata.jp/5/cptop.html",
-        "https://baseballdata.jp/6/cptop.html",
-        "https://baseballdata.jp/7/cptop.html",
-        "https://baseballdata.jp/8/cptop.html",
-        "https://baseballdata.jp/9/cptop.html",
-        "https://baseballdata.jp/11/cptop.html",
-        "https://baseballdata.jp/12/cptop.html",
-        "https://baseballdata.jp/376/cptop.html",
+        # "https://baseballdata.jp/2/cptop.html",
+        # "https://baseballdata.jp/3/cptop.html",
+        # "https://baseballdata.jp/4/cptop.html",
+        # "https://baseballdata.jp/5/cptop.html",
+        # "https://baseballdata.jp/6/cptop.html",
+        # "https://baseballdata.jp/7/cptop.html",
+        # "https://baseballdata.jp/8/cptop.html",
+        # "https://baseballdata.jp/9/cptop.html",
+        # "https://baseballdata.jp/11/cptop.html",
+        # "https://baseballdata.jp/12/cptop.html",
+        # "https://baseballdata.jp/376/cptop.html",
     ]
 
 # 辞書を取得
